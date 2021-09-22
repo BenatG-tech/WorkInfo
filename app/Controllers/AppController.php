a0cb858ac582059e52a9349ed71581dff2cafe92
Beñat Garcia <benat.dev@gmail.com>
2021-09-22 12:34:00 +0200

    Cambios menores en la configuración y diseño inicial de página de inicio realizado al 80%.

---
+++ app/Controllers/AppController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class AppController extends BaseController
+{
+	public function index()
+	{
+		return view('app');
+	}
+}
---
+++ app/Views/app.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link href="styles/inicio.css" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+	<h1>WORK INFO</h1>
+	<div class="menu_principal">
+		<a href="" target="_blank"><button class="boton_menu_principal">Calendario</button><a>
+		<button class="boton_menu_principal">Registro de Horas</button>
+		<button class="boton_menu_principal">Horas libres</button>
+		<button class="boton_menu_principal">Hola</button>
+	</div>
+</header>
+
+<div class="contenido">
+	<!--?= $this->renderSection('content') ?-->
+	<div class="menu_izquierda">
+		<h1>Esto es el menú de la izquierda.</h1>
+	</div>
+	<div class="contenido_derecha">
+		<h1>Esto es el contenido de la derecha.</h1>
+	</div>
+</div>
+
+<footer>
+	<div>
+	</div>
+</footer>
+
+</body>
+</html>
